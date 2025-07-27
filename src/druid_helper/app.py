c4afcbe72aaeec170746cd3300395a13ad8a5016
Medusa Medium <a@b.c>
2025-07-27 15:19:54 -0400

    fixed import

--- src/druid_helper/app.py
+++ src/druid_helper/app.py
@@ -6,7 +6,7 @@ from textual.containers import Container, Vertical, VerticalGroup
 from textual.widget import Widget
 from textual.widgets import Footer, Header, RichLog
 
-from src.druid_helper.listing import Listing
+from druid_helper.listing import Listing
 
 logger = logging.getLogger(__name__)
 
