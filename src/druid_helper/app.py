2bf7f18b1b8be34320882a421d190ef7412a7be6
Medusa Medium <a@b.c>
2025-07-27 15:30:33 -0400

    populating body object

--- src/druid_helper/app.py
+++ src/druid_helper/app.py
@@ -22,23 +22,29 @@ class LoggingConsole(RichLog):
 class Body(Vertical):
     """Application body."""
 
-
-class DruidHelper(App):
-    CSS_PATH = "layout.tcss"
-
     def __init__(self, rich_log_handler: RichHandler, *args, **kwargs):
         self.rich_log_handler = rich_log_handler
         super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
+        yield Listing(name="Listing", id="listing")
+        with Container(name="Details", id="details", classes="box"):
+            with VerticalGroup(name="Logs", id="logs", classes="box"):
+                # yield RichLog()
+                # yield Log()
+                yield self.rich_log_handler.console  # type: ignore
+
+
+class DruidHelper(App):
+    CSS_PATH = "layout.tcss"
+
+    def __init__(self, rich_log_handler: RichHandler, *args, **kwargs):
+        self.body = Body(rich_log_handler, id="body")
+        super().__init__(*args, **kwargs)
+
+    def compose(self) -> ComposeResult:
         yield Header()
-        with Body(id="body"):
-            yield Listing(name="Listing", id="listing")
-            with Container(name="Details", id="details", classes="box"):
-                with VerticalGroup(name="Logs", id="logs", classes="box"):
-                    # yield RichLog()
-                    # yield Log()
-                    yield self.rich_log_handler.console  # type: ignore
+        yield self.body
         yield Footer()
 
     def on_mount(self) -> None:
