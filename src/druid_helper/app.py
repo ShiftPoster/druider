48b21e70fade4f296380f9f2257e659d95c6ca74
Medusa Medium <a@b.c>
2025-07-27 15:18:40 -0400

    added logging pane

--- src/druid_helper/app.py
+++ src/druid_helper/app.py
@@ -1,23 +1,60 @@
-from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Footer, Header, Static, Log
+import logging
 
-from druid_helper.table import Animals
+from textual.widget import Widget
+from textual.app import App, ComposeResult
+from textual.containers import Container, VerticalGroup, Vertical
+from textual.widgets import Footer, Header, RichLog
+from rich.logging import RichHandler
+
+from src.druid_helper.listing import Listing
+
+logger = logging.getLogger(__name__)
+
+
+class LoggingConsole(RichLog):
+    file = False
+    console: Widget
+
+    def print(self, content):
+        self.write(content)
+
+
+class Body(Vertical):
+    ...
 
 
 class DruidHelper(App):
     CSS_PATH = "layout.tcss"
 
+    def __init__(self, rich_log_handler: RichHandler, *args, **kwargs):
+        self.rich_log_handler = rich_log_handler
+        super().__init__(*args, **kwargs)
+
     def compose(self) -> ComposeResult:
         yield Header()
-        with Container(id="app-grid"):
-            with Static(name="Listing", id="sidebar"):
-                yield Animals()
+        with Body(id="body"):
+            yield Listing(name="Listing", id="listing")
             with Container(name="Details", id="details", classes="box"):
-                yield Log()
+                with VerticalGroup(name="Logs", id="logs", classes="box"):
+                    # yield RichLog()
+                    # yield Log()
+                    yield self.rich_log_handler.console  # type: ignore
         yield Footer()
+
+    def on_mount(self) -> None:
+        logger.info("asdfasdfasdf")
 
 
 if __name__ == "__main__":
-    app = DruidHelper()
+    from textual.logging import TextualHandler
+
+    root = logging.getLogger()
+    root.setLevel(1)
+    rich_log_handler = RichHandler(
+        console=LoggingConsole(),  # type: ignore
+        rich_tracebacks=True,
+    )
+    root.addHandler(rich_log_handler)
+    root.addHandler(TextualHandler())
+    app = DruidHelper(rich_log_handler)
     app.run()
